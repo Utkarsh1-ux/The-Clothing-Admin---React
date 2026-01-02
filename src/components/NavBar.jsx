06a0adb5ec1086c0c49b5a19077a1ae29204ba5f
Utkarsh1-ux <utkarshramu53@gmail.com>
2026-01-02 11:05:55 +0530

    added tailwind and come components

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {assets} from '../assets/assets.js'
-const NavBar = () => {
+const NavBar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <img className='w-[max(15%,80px)]' src={assets.logo} alt="" />
-        <button className='cursor-pointer bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+        <button onClick={()=>setToken('')} className='cursor-pointer bg-blue-950 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
 }
--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import React from "react";
+import { assets } from "../assets/assets";
 
 const Add = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <form className="flex flex-col w-full items-start gap-3">
+      <div>
+        <p className="mb-2">Upload Image</p>
 
-export default Add
+        <div className="flex gap-2">
+          <label htmlFor="image1">
+            <img className="w-20" src={assets.upload_area} alt="" />
+            <input type="file" id="image1" hidden />
+          </label>
+          <label htmlFor="image2">
+            <img className="w-20" src={assets.upload_area} alt="" />
+            <input type="file" id="image2" hidden />
+          </label>
+          <label htmlFor="image3">
+            <img className="w-20" src={assets.upload_area} alt="" />
+            <input type="file" id="image3" hidden />
+          </label>
+          <label htmlFor="image4">
+            <img className="w-20" src={assets.upload_area} alt="" />
+            <input type="file" id="image4" hidden />
+          </label>
+        </div>
+      </div>
+
+        <div className="w-full">
+        <p className="mb-2">Product name</p>
+        <input className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Enter Product name" required />
+        </div>
+
+        <div className="w-full">
+        <p className="mb-2">Product description</p>
+        <textarea className="w-full max-w-[500px] px-3 py-2" type="text" placeholder="Write your content" required />
+        </div>
+
+        <div>
+          <div>
+            <p>Product category</p>
+            <select>
+              <option value="Men">Men</option>
+              <option value="Women">Women</option>
+              <option value="Kids">Kids</option>
+            </select>
+          </div> 
+
+          <div>
+            <p>Sub category</p>
+            <select>
+              <option value="Topwear">Topwear</option>
+              <option value="Bottomwear">Bottomwear</option>
+              <option value="Winterwear">Winterwear</option>
+            </select>
+          </div>
+
+          <div>
+            <p>Product Price</p>
+            <input type="Number" placeholder="20" />
+          </div>
+        </div>
+    </form>
+  );
+};
+
+export default Add;
