5e258c19f53780e27f306d665b4578f2411c9ba5
Utkarsh1-ux <utkarshramu53@gmail.com>
2026-01-06 17:39:27 +0530

    added list

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { backendUrl } from '../App'
 
 const List = () => {
+
+ const [list , setList] = useState([])
+
+ const fetchList = async () =>{
+    try {
+           
+       const response = await axios.get(backendUrl + '/api/product/list')
+       console.log(response.data);
+       
+    } catch (error) {
+      
+    }
+ }
+
+ useEffect(()=>{
+    fetchList();
+ },[])
+
+
   return (
     <div>
         
